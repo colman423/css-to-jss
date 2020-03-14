8204e3c376f20eca9a91a3a8dba2fafe807ef8e6
colman423 <colman0423@gmail.com>
2020-03-14 21:15:42 +0800

    Updates

---
+++ precache-manifest.58c1907faa74bd30c1110f8f9998e9be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a7a86457bd0a26594da87fe4c4075e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fa63d6824921d23ab77",
+    "url": "/static/js/2.501f4b03.chunk.js"
+  },
+  {
+    "revision": "aa762385ab423a29206547471fcc4bac",
+    "url": "/static/js/2.501f4b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48630bef23a12aa12183",
+    "url": "/static/js/main.830b115a.chunk.js"
+  },
+  {
+    "revision": "25fb8f4f28d30951bf32",
+    "url": "/static/js/runtime-main.7befb7f6.js"
+  }
+]);
