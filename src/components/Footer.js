4f010e3618a8d046620b226f5b2475d785bfad5a
colman423 <colman0423@gmail.com>
2020-03-15 19:02:09 +0800

    href settings, service worker

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer({ className, ...props }) {
   return (
     <div className={className}>
-      <a href="https://github.com/colman423/css-to-jss">github link.</a>
+      <a href="https://github.com/colman423/css-to-jss" target="_blank">github link.</a>
     </div>
   )
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ function Title({ className, ...props }) {
   return (
     <div className={className}>
       <h1>Css to Jss Converter</h1>
-      <p>A simple tool to convert Css to Jss. <a href="https://cssinjs.org">About Jss</a></p>
+      <p>A simple tool to convert Css to Jss. <a href="https://cssinjs.org" target="_blank" rel="noopener noreferrer">About Jss</a></p>
     </div>
   )
 }
